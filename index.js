bb0860f65f330a4a4c803e4c216bc9e93cb954d5
PersonneMascotte <raphael.bottoli@gmail.com>
2021-02-26 10:00:10 +0100

    delete prefix index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require("./config.json")
-prefixe = "@"
 
 //const (nom de la commande) = require("lien vers le fichier ex : ./commandes/.");
 const commandes = require("./commandes/commandes");
